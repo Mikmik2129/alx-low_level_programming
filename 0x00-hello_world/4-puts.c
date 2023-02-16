a771eb8cccc1a17661f1836c8bc9b98fa727e54f
Mikmik2129 <bayabanana12@gmail.com>
2023-02-16 06:57:46 -0800

    printing the file using puts command

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/** 
+ * main - prints the phrase "Programming is like buliding a multilingual puzzle
+ * REturn: 0 if existed properly, non-zero otherwise
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
